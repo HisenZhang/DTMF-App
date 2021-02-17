80fd6931e14d147305c87b34fd8bc3974c390f9c
Hisen Zhang <zhangz29@rpi.edu>
2021-02-16 19:40:07 -0500

    allow offset

--- handler/dynamic.py
+++ handler/dynamic.py
@@ -1,9 +1,13 @@
 import datetime
 
+HOUR_OFFSET = -1
+MINUTE_OFFSET = +2
+
 def on_monitor(DTMFString: str) -> None:
     print("You entered: " + DTMFString)
     t = datetime.datetime.now()
-    TOKEN = str(t.hour).zfill(2) + str(t.minute).zfill(2)
+    TOKEN = str(t.hour + HOUR_OFFSET).zfill(2) + \
+            str(t.minute + MINUTE_OFFSET).zfill(2)
     if DTMFString == TOKEN:
         print("Access granted.")
     else:
